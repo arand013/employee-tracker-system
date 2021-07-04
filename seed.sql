f87df0829b3c9799ea0290b630c94bc362e33f38
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-03 18:01:57 -0700

    seeds sql setup

---
+++ seed.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE DATABASE employeesDB;
+USE employeesDB;
+
+CREATE TABLE department
+(id INT PRIMARY KEY AUTO_INCREMENT,
+department_name VARCHAR(30)
+);
+
+CREATE TABLE roles
+(id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(10,2),
+department_id INT
+);
+
+CREATE TABLE employee
+(id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
+);
+
+/* !!!CHANGE COLUMN NAMES IF DIFFERENT!!! */
+INSERT INTO department (department_name) VALUES
+("Manager"), ("Engineer"), ("Intern");
+
+INSERT INTO roles (title, salary, department_id) VALUES
+("Manager", 100000.00, 1), ("Engineer", 90000.00, 2), ("Intern", 0.00, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Harrison", "Schaen", 1, NULL),
+("Steph", "Huynh", 2, 1),
+("Steven", "Landgraf", 2, 1),
+("Daniel", "Angulo", 3, 2),
+("Said", "Aguilar", 3, 2),
+("Matthew", "Bell", 3, 2),
+("Julian", "Acosta", 3, 2),
+("Tyler", "Arthur", 3, 2),
+("Javier", "Banuelos", 3, 2),
+("Art", "Aragon", 3, 2),
+("Cara", "Bunnell", 3, 2),
+("Jaycee", "Bagtas", 3, 3),
+("Alexia", "Chalita", 3, 3),
+("Lizbeth", "Glasser", 3, 3),
+("Collin", "Kier", 3, 3),
+("Brian", "Monteverde", 3, 3),
+("Greg", "Schuman", 3, 3),
+("Amy", "Fabella", 3, 3),
+("Jason", "Riley", 3, 3);
